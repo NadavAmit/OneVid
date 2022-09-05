e26d1c267fbb16fbc5d216bd7cc7ae39e5c53ddb
Amit Nadav <amit.nadav1@gmail.com>
2022-09-06 01:01:05 +0300

    project init

---
+++ backend/src/main/java/com/nadav/onevid/OnevidApplication.java
@@ -0,0 +1,13 @@
+package com.nadav.onevid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnevidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnevidApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nadav/onevid/OnevidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadav.onevid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnevidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
